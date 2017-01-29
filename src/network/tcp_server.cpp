5cf16ea3c1290d076cee2c626203060dee307f4a
ユーマヨ <scha-taz0@outlook.jp>
2017-01-30 03:15:44 +0900

    複数のClientに対応したときに、最後のソケットが壊れる不具合を修正。
    for文の中にio.run()が含まれていたので、外に出しました。
    closeしていないsocketをacceptした時に例外が発生。
    必ずcloseしましょう。
    またclientがserverから離脱したときにsocketを再接続するようにしました。

--- src/network/tcp_server.cpp
+++ src/network/tcp_server.cpp
@@ -31,12 +31,55 @@ struct tcp_server::_member
             sockets.emplace_back( io );
         }
     }
+    void async_accept( socket_object& sock_obj );
     asio::io_service io;
     tcp::acceptor acceptor;
     std::vector<socket_object> sockets;
-    bool stop = false;
     std::shared_ptr<std::thread> thread;
 };
+void tcp_server::_member::async_accept( socket_object& sock_obj )
+{
+    log( "socket accept" );
+    acceptor.async_accept( sock_obj.socket, [ this, &sock_obj ] ( asio::error_code const& error )
+    {
+        if ( error )
+        {
+            log( "accept failed: %s", error.message( ).c_str( ) );
+        }
+        else
+        {
+            log( "accept correct!" );
+
+            asio::async_read(
+                sock_obj.socket,
+                asio::buffer( sock_obj.buffer ),
+                asio::transfer_at_least( 4 ),
+                [ this, &sock_obj ] ( const asio::error_code& error, size_t bytes_transferred )
+            {
+                if ( error )
+                {
+                    if ( error == asio::error::eof )
+                    {
+                        log( "client close: %s", error.message( ).c_str( ) );
+
+                        // クライアントがいなくなったソケットは、もう一度接続します。
+                        sock_obj.socket.close( );
+                        async_accept( sock_obj );
+                    }
+                    else
+                    {
+                        log( "receive failed: %s", error.message( ).c_str( ) );
+                    }
+                }
+                else
+                {
+                    const char* data = sock_obj.buffer.data( );
+                    log( "receive data: %s", data );
+                }
+            } );
+        }
+    } );
+}
 CREATE_CPP( tcp_server, std::string const& port, int num_of_client )
 {
     CREATE( tcp_server, port, num_of_client );
@@ -44,7 +87,6 @@ CREATE_CPP( tcp_server, std::string const& port, int num_of_client )
 tcp_server::~tcp_server( )
 {
     _m->io.stop( );
-    _m->stop = true;
     _m->thread->join( );
 }
 bool tcp_server::init( std::string const& port, int num_of_client )
@@ -54,41 +96,11 @@ bool tcp_server::init( std::string const& port, int num_of_client )
 
     _m->thread = std::make_shared<std::thread>( [ this ]
     {
-        while ( !_m->stop )
+        for ( auto& obj : _m->sockets )
         {
-            for ( auto& obj : _m->sockets )
-            {
-                _m->acceptor.async_accept( obj.socket, [ this, &obj ] ( asio::error_code const& error )
-                {
-                    if ( error )
-                    {
-                        log( "accept failed: %s", error.message( ).c_str( ) );
-                    }
-                    else
-                    {
-                        log( "accept correct!" );
-
-                        asio::async_read(
-                            obj.socket,
-                            asio::buffer( obj.buffer ),
-                            asio::transfer_at_least( 4 ),
-                            [ this, &obj ] ( const asio::error_code& error, size_t bytes_transferred )
-                        {
-                            if ( error && error != asio::error::eof )
-                            {
-                                log( "receive failed: %s", error.message( ).c_str( ) );
-                            }
-                            else
-                            {
-                                const char* data = obj.buffer.data( );
-                                log( "receive data: %s", data );
-                            }
-                        } );
-                    }
-                } );
-            }
-            _m->io.run( );
+            _m->async_accept( obj );
         }
+        _m->io.run( );
     } );
     return true;
 }
