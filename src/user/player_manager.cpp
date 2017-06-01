bf8d141592a39625ce38ceb06eab6bb4844ef808
ユーマヨ <scha-taz0@outlook.jp>
2017-06-02 01:00:26 +0900

    スキンを選択してプレイできるようになりました。
    ネットワークにも対応させました。

--- src/user/player_manager.cpp
+++ src/user/player_manager.cpp
@@ -7,7 +7,8 @@ namespace user
 {
 void player_manager::create_client( Json::Value const & root_client )
 {
-    auto enemy = player::create( root_client["ip_address"].asString( ), root_client["port"].asInt( ), "" );
+    auto enemy = player::create( root_client["ip_address"].asString( ), root_client["port"].asInt( ),
+                                 root_client["select_skin_name"].asString( ).empty( ) ? "" : "skin/" + root_client["select_skin_name"].asString( ) + ".png" );
     enemy->set_color( { 0.6F, 0.2F, 0.8F } );
     enemy->set_name( "enemy" );
     _clients.lock( )->add_child( enemy );
@@ -42,10 +43,11 @@ bool player_manager::init( Json::Value& root, std::shared_ptr<network::udp_conne
     _clients = clients;
     add_child( clients );
 
-    auto& skin_name_value = user_default::get_instans( )->get_root( )["select"];
+    auto& skin_name_value = user_default::get_instans( )->get_root( )["select_skin_name"];
 
     auto& j = root["data"];
-    auto player = player::create( j["ip_address"].asString( ), j["port"].asInt( ), "" );
+    auto player = player::create( j["ip_address"].asString( ), j["port"].asInt( ), 
+                                  skin_name_value.asString( ).empty( ) ? "" : "skin/" + skin_name_value.asString( ) + ".png" );
     _player = player;
     player->set_color( { 0.2F, 0.8F, 0.6F } );
     player->set_name( "own" );
--- src/user/search_room.cpp
+++ src/user/search_room.cpp
@@ -11,39 +11,35 @@ CREATE_CPP( search_room )
 bool search_room::init( )
 {
     set_name( "search_room" );
-    Json::Value root;
-    if ( Json::Reader( ).parse( cinder::app::loadString( "udp.json" ), root ) )
+
+    auto client = network::udp_connection::create( );
+    _client = client;
+    add_child( client );
+    client->set_name( "connection" );
+    client->on_receive_failed = [ this ] ( )
     {
-        std::string address = root["server"]["address"].asString( );
-        user_default::get_instans( )->get_root( )["server"]["address"] = address;
-        int port = boost::lexical_cast<int>( root["server"]["port"].asString( ) );
-        user_default::get_instans( )->get_root( )["server"]["port"] = port;
+        scene_manager::get_instans( )->top( )->set_block_schedule_event( false );
+        if ( on_not_found ) on_not_found( );
 
-        auto client = network::udp_connection::create( );
-        _client = client;
-        add_child( client );
-        client->set_name( "connection" );
-        client->on_receive_failed = [ this ] ( )
-        {
-            scene_manager::get_instans( )->top( )->set_block_schedule_event( false );
-            if ( on_not_found ) on_not_found( );
+        remove_from_parent( );
+    };
+    client->on_received_json = [ this ] ( network::network_handle handle, Json::Value root )
+    {
+        if ( on_founded ) on_founded( root );
+        auto node = scene_manager::get_instans( )->get_dont_destroy_node( );
+        _client.lock( )->set_parent( node.lock( ) );
+        _client.lock( )->on_received_json = nullptr;
+        _client.lock( )->on_receive_failed = nullptr;
+        _client.lock( )->set_schedule_update( false );
+    };
 
-            remove_from_parent( );
-        };
-        client->on_received_json = [ this ] ( network::network_handle handle, Json::Value root )
-        {
-            if ( on_founded ) on_founded( root );
-            auto node = scene_manager::get_instans( )->get_dont_destroy_node( );
-            _client.lock( )->set_parent( node.lock( ) );
-            _client.lock( )->on_received_json = nullptr;
-            _client.lock( )->on_receive_failed = nullptr;
-            _client.lock( )->set_schedule_update( false );
-        };
-        
-        client->write( std::make_shared<network::network_object>( address, port ),
-                       "{\"name\":\"find_room\"}" );
-        return true;
-    }
-    return false;
+    Json::Value root;
+    root["name"] = "find_room";
+    root["data"]["select_skin_name"] = user_default::get_instans( )->get_root( )["data"]["select_skin_name"].asString( );
+    auto address = user_default::get_instans( )->get_root( )["server"]["address"].asString( );
+    auto port = user_default::get_instans( )->get_root( )["server"]["port"].asInt( );
+    client->write( std::make_shared<network::network_object>( address, port ),
+                   Json::FastWriter( ).write( root ) );
+    return true;
 }
 }
